b579f8fcf36e27e30edce1dfca9e1aa5fed75863
Connor M <connormitchell80@gmail.com>
2016-09-19 08:46:10 -0500

    fixed score problems

--- perquackeyscore.py
+++ perquackeyscore.py
@@ -12,7 +12,7 @@ six = []
 seven = []
 eight = []
 #Append the number after the "<=" in the following line to change the amount of time given. It's in seconds
-while time.time() - starttime <= 10:
+while time.time() - starttime <= 180:
     word = str(raw_input("Word: "))
     wordcount = len(list(word))
 
@@ -80,57 +80,57 @@ while time.time() - starttime <= 10:
         elif wordcount == 6:
             six.append(word)
             if len(six) == 1:
-                points += 120
+                points += 300
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(six) == 2:
-                points += 140
+                points += 400
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(six) == 3:
-                points += 160
+                points += 500
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(six) == 4:
-                points += 180
+                points += 600
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(six) == 5:
-                points += 200
+                points += 700
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(six) >= 5:
                 print "Too many three six letter words! No points awarded"
         elif wordcount == 7:
             seven.append(word)
             if len(seven) == 1:
-                points += 120
+                points += 500
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(seven) == 2:
-                points += 140
+                points += 650
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(seven) == 3:
-                points += 160
+                points += 800
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(seven) == 4:
-                points += 180
+                points += 950
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(seven) == 5:
-                points += 200
+                points += 1100
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(seven) >= 5:
                 print "Too many seven letter words! No points awarded"
-        elif wordcount == 8:
+        elif wordcount >= 8:
             eight.append(word)
             if len(eight) == 1:
-                points += 120
+                points += 750
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(eight) == 2:
-                points += 140
+                points += 1000
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(eight) == 3:
-                points += 160
+                points += 1250
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(eight) == 4:
-                points += 180
+                points += 1500
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(eight) == 5:
-                points += 200
+                points += 1750
                 print "Good! Current time is",time.time() - starttime,", and your current score is", points
             elif len(eight) >= 5:
                 print "Too many eight letter words! No points awarded"
